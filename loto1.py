d00beb9255169c7f31ecca566f41cc366549702f
Elerinna2a <elerinna2a@gmail.com>
2022-10-27 07:50:01 +0200

    Create loto1.py

---
+++ loto1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 20:21:53 2022
+
+@author: Antony
+"""
+
+import random
+liste1=list(range(1,50))
+liste2= []
+
+def tirage(liste1):
+    a=liste1[random.randint(1,len(liste1))]
+    liste1.remove(a)
+    liste2.append(a)
+    return liste2
+# print(tirage(liste1))
+
+def loto(liste1):
+    for i in range(5):
+        tirage(liste1)
+    return liste2
+print(loto(liste1))
