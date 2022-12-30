a77fcd76ad2add16c184547da7deda95e7a42cb6
Elerinna2a <elerinna2a@gmail.com>
2022-12-30 09:33:30 +0100

    Create loto.py

---
+++ loto.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 19:57:39 2022
+
+@author: Antony
+"""
+
+import random
+liste1=list(range(1,50))
+
+def tirage(liste1):
+    a=liste1[0]
+    a=random.randint(1,50)
+tirage(liste1)
